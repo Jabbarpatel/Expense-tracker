2997b995b57922f4bc7f2bbf236f259f1440581a
Jabbar <j.patel@bm-systems.in>
2026-04-30 10:24:26 +0300

    added axios configuartion

---
+++ web/frontend/src/api/axios.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const IS_DEV: boolean = true;
+const BACKEND_PORT: number = 8000;
+
+const API_URL: string = IS_DEV
+  ? `${window.location.origin.replace(/:\d+?$/, "")}:${BACKEND_PORT}/api`
+  : `${window.location.origin}/api`;
+
+export const customApi = axios.create({
+  baseURL: API_URL,
+  headers: { "Content-Type": "application/json" },
+});
+
+customApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    config.headers.Authorization = "";
+  }
+  return config;
+});
